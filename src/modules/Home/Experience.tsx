d70832e8b5a44a6c4270a529e9b8ae754324ae18
fahim-khandakar <fahimkhandakar01@gmail.com>
2025-10-19 23:27:26 +0600

    about us section added

--- src/modules/Home/Experience.tsx
+++ src/modules/Home/Experience.tsx
@@ -2,13 +2,34 @@ import Image from "next/image";
 import worldBg from "@/assets/block-wrapper.jpg";
 
 export default function Experience() {
+  const data = [
+    { count: "20", details: "Years\nof Experience" },
+    { count: "2.9k", details: "Vehicle\nRepaired" },
+    { count: "32", details: "Technicians &\nWorkers" },
+    { count: "100%", details: "Satisfied\nCustomers" },
+  ];
+
   return (
-    <div>
+    <section className="relative">
       <Image
-        className="w-full h-full"
+        className="w-full h-full object-cover"
         src={worldBg}
         alt="World Background Image"
       />
-    </div>
+      <div className="absolute inset-0 flex justify-center items-center">
+        <div className="flex gap-10 flex-col md:flex-row ">
+          {data.map((item, idx) => (
+            <p key={idx} className="text-white whitespace-pre-line flex">
+              <p className="border-r-2 border-amber-500 pr-4 text-4xl md:text-6xl font-bold">
+                {item.count}
+              </p>{" "}
+              <p className="font-semibold text-lg md:text-xl pl-5">
+                {item.details}
+              </p>
+            </p>
+          ))}
+        </div>
+      </div>
+    </section>
   );
 }
